41117bf91d644f0a62d9c0b6689e2297ae5da237
connor <connordanielfrank@gmail.com>
2023-02-20 18:25:59 +1300

    dev: Add manufacturer model schema

---
+++ models/manufacturer.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ManufacturerSchema = new Schema({
+    name: { type: String, required: true },
+    country: { type: String, required: true },
+});
+
+ManufacturerSchema.virtual('url').get(function () {
+    return `/manufacturer/${this._id}`;
+}
+
+module.exports = mongoose.model('Manufacturer', ManufacturerSchema);
