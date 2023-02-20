ffca8ad8a83f6ad19bb0e10b550a3ba296decdd1
connor <connordanielfrank@gmail.com>
2023-02-21 09:32:28 +1300

    dev: Complete ammoController

---
+++ controllers/ammoController.js
@@ -0,0 +1,22 @@
+const Ammo = require('../models/ammo');
+
+// Display list of all ammo
+exports.ammo_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Ammo list');
+};
+
+exports.ammo_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Ammo create GET');
+};
+
+exports.ammo_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Ammo create POST');
+};
+
+exports.ammo_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Ammo delete GET');
+};
+
+exports.ammo_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Ammo delete POST');
+};
