53a55bd3d0de8555622b07df5a817a22429cda18
connor <connordanielfrank@gmail.com>
2023-02-21 13:48:19 +1300

    dev: Add ammoinstanceController

---
+++ controllers/ammoinstanceController.js
@@ -0,0 +1,21 @@
+const Ammoinstance = require('../models/ammoinstance');
+
+exports.ammoinstance_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Ammoinstance list');
+}
+
+exports.ammoinstance_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Ammoinstance create GET');
+}
+
+exports.ammoinstance_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Ammoinstance create POST');
+}
+
+exports.ammoinstance_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Ammoinstance delete GET');
+}
+
+exports.ammoinstance_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Ammoinstance delete POST');
+}
