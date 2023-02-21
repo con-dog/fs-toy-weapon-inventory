7565ad1482c64298f3ba16ca3e06a0db2a8247b9
connor <connordanielfrank@gmail.com>
2023-02-21 13:50:31 +1300

    dev: Add classController skeleton

---
+++ controllers/classController.js
@@ -0,0 +1,33 @@
+const Class = require('../models/classModel');
+
+exports.class_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Class list');
+}
+
+exports.class_detail = (req, res) => {
+  res.send('NOT IMPLEMENTED: Class detail: ' + req.params.id);
+}
+
+exports.class_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Class create GET');
+}
+
+exports.class_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Class create POST');
+}
+
+exports.class_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Class delete GET');
+}
+
+exports.class_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Class delete POST');
+}
+
+exports.class_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Class update GET');
+}
+
+exports.class_update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Class update POST');
+}
