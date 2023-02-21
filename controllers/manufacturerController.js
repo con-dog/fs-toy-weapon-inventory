34f0d6420ca16df0a6566f603465ab16c4a5df48
connor <connordanielfrank@gmail.com>
2023-02-21 13:53:31 +1300

    dev: Add manufacturerController.js skeleton

---
+++ controllers/manufacturerController.js
@@ -0,0 +1,33 @@
+const Manufacturer = require('../models/manufacturer');
+
+exports.manufacturer_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Manufacturer list');
+}
+
+exports.manufacturer_detail = (req, res) => {
+  res.send('NOT IMPLEMENTED: Manufacturer detail: ' + req.params.id);
+}
+
+exports.manufacturer_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Manufacturer create GET');
+}
+
+exports.manufacturer_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Manufacturer create POST');
+}
+
+exports.manufacturer_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Manufacturer delete GET');
+}
+
+exports.manufacturer_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Manufacturer delete POST');
+}
+
+exports.manufacturer_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Manufacturer update GET');
+}
+
+exports.manufacturer_update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Manufacturer update POST');
+}
